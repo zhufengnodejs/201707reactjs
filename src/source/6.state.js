88fb273b716d90754dcc55dac82078cd072ae26f
zhufengnodejs <zhufengnodejs@126.com>
2017-09-23 15:29:52 +0800

    clock

--- src/source/6.state.js
+++ src/source/6.state.js
@@ -6,7 +6,20 @@ class Clock extends Component{
     super();
     this.state = {date:new Date()};//初始化默认状态
   }
+  componentWillMount(){
+    console.log('1. componentWillMount 组件将要挂载!');
+  }
+  componentDidMount(){
+    console.log('3. componentDidMount 组件挂载完成!');
+    this.timer = setInterval(()=>{
+      this.setState({date:new Date()});
+    },1000);
+  }
+  componentWillUnmount(){
+    console.log('4. componentWillUnmount 组件将要卸载!');
+  }
   render(){
+    console.log('2. render 挂载动作!');
     return (
       <div>
         <h1>Hello, world!</h1>
@@ -15,12 +28,7 @@ class Clock extends Component{
     );
   }
 }
-
-function tick() {
-  ReactDOM.render(
-    <Clock/>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Clock/>,
+  document.getElementById('root')
+);
