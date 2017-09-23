c195eedce5942c01a7117407fb3b63e5a98f7a92
zhufengnodejs <zhufengnodejs@126.com>
2017-09-23 12:31:30 +0800

    clock

--- src/source/5.comment.js
+++ src/source/5.comment.js
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom';
 function formatDate(date){
   return date.toLocaleDateString();
 }
-function Avatar({name,avatarUrl}){
+function Avatar({author}){
   return (
     <img style={{width:'25px',height:'25px',borderRadius:'5px'}} className="Avatar"
-         src={avatarUrl}
-         alt={name}
+         src={author.avatarUrl}
+         alt={author.name}
     />
+  )
+}
+function UserInfo(props){// = {author}
+  return (
+    <div className="UserInfo">
+      <Avatar author={props.author}/>
+      <div className="UserInfo-name">
+        {props.author.name}
+      </div>
+    </div>
   )
 }
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <Avatar {...props.author}/>
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo author={props.author}/>
       <div className="Comment-text">
         {props.text}
       </div>
