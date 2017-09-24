8bfbdaca2e2a6566fec51eda08e8af00dfd938e2
zhufengnodejs <zhufengnodejs@126.com>
2017-09-24 16:11:05 +0800

    clock

---
+++ src/components/Slider/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import './index.css'
+export default class Slider extends Component{
+    render(){
+        return (
+            <div className="slider-wrapper">
+
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from './components/Slider';
 let IMAGES = [
-  require('./1.jpg'),
-  require('./2.jpg'),
-  require('./3.jpg')
+  require('./images/1.jpg'),
+  require('./images/2.jpg'),
+  require('./images/3.jpg')
 ]
 ReactDOM.render(<Slider
   images={IMAGES} //需要轮播的图片
