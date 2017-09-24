9473ace8c03ad66c2b1d66c4cf8851cf25ddd075
zhufengnodejs <zhufengnodejs@126.com>
2017-09-24 17:02:21 +0800

    clock

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -5,6 +5,25 @@ export default class Slider extends Component {
     super();
     this.state = {index:0};
   }
+  componentDidMount(){
+    if(this.props.auto){//如果要自动启动轮播的话
+        this.go();//开始轮播
+    }
+  }
+  go = ()=>{
+    this.timer = setInterval(()=>{
+      this.turn(1);
+    },this.props.delay*1000);
+  }
+  //step是步长，走几步
+  turn = (step)=>{
+    let index = this.state.index;
+    index+=step;
+    if(index>=this.props.images.length){
+      index = 0;
+    }
+    this.setState({index});
+  }
   render() {
     let style = {
       left:-500*this.state.index+'px',
