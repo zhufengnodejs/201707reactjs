66faf14815bc38df28c896c868b2a016520dea3e
zhufengnodejs <zhufengnodejs@126.com>
2017-09-24 16:30:44 +0800

    clock

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import './index.css'
 export default class Slider extends Component {
+  constructor(){
+    super();
+    this.state = {index:0};
+  }
   render() {
+    let style = {
+      left:-500*this.state.index+'px',
+      width:500*this.props.images.length+'px',
+      transitionDuration:this.props.speed+'s'
+    }
     return (
       <div className="slider-wrapper">
-        <ul className="sliders">
+        <ul style={style} className="sliders">
           {
             this.props.images.map((image,index)=>(
               <li className="slider">
