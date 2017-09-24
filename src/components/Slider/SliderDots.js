5f4edf6df2715320c46c7826a65fa98491c771ee
zhufengnodejs <zhufengnodejs@126.com>
2017-09-24 17:59:12 +0800

    clock

--- src/components/Slider/SliderDots.js
+++ src/components/Slider/SliderDots.js
@@ -5,7 +5,7 @@ export default class SliderDots extends Component {
       <div className="dots">
         {
           this.props.images.map((item,index)=>(
-            <span className="dot"
+            <span className={"dot "+(this.props.index==index?"active":"")}
                   onClick={()=>this.props.turn(index-this.props.index)}
             >{index+1}</span>
           ))
