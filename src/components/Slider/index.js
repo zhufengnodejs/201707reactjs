c3f137bfae1e789c83781c32758ca29fa59c773a
zhufengnodejs <zhufengnodejs@126.com>
2017-09-24 16:19:07 +0800

    clock

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,11 +1,19 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './index.css'
-export default class Slider extends Component{
-    render(){
-        return (
-            <div className="slider-wrapper">
-
-            </div>
-        )
-    }
+export default class Slider extends Component {
+  render() {
+    return (
+      <div className="slider-wrapper">
+        <ul className="sliders">
+          {
+            this.props.images.map((image,index)=>(
+              <li className="slider">
+                <img src={image} />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
 }
